ecb43fc2db0bab181539676425f94dbd2670570a
Erin Ewart <ems1980@hotmail.com>
2022-02-15 20:52:17 -0500

    addressed usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,16 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-     // package up data as an object
-  var taskDataObj = {
+     // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+      alert("You need to fill out the task form!");
+      return false;
+    }
+    
+    formEl.reset();
+
+    // package up data as an object
+    var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
   };
